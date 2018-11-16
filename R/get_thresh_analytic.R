a12f5417dbf28e2d1053ad8c3127974211085f85
Kelsey Grinde <kelseyegrinde@gmail.com>
2018-11-16 15:43:26 -0800

    change to read map as input

--- R/get_thresh_analytic.R
+++ R/get_thresh_analytic.R
@@ -5,9 +5,7 @@
 #' see Grinde et al. (2018).
 #'
 #' @param g the generations since admixture.
-#' @param delt the average spacing (cM per marker) between markers.
-#' @param L the total length (cM) of the region.
-#' @param chr the number of chromosomes; default is 22.
+#' @param map data frame with m rows and at least 2 columns ('chr' containing chromosome number and 'cM' containing genetic position in centimorgans), where m = no. markers
 #' @param alpha the level for family-wise error rate control; default is 0.05.
 #' @param type the type of threshold that should be returned: \code{"stat"} for test statistic or \code{"pval"} for p-value; defaults to pval.
 #' @param searchint the range of test statistic thresholds for \code{uniroot} to search; defaults to 1.96--8, corresponding to p-value threshold of 0.05--1.2e-15
@@ -23,8 +21,14 @@
 #' @importFrom stats uniroot pnorm
 #'
 #' @export
-
-get_thresh_analytic <- function(g, delt, L, chr = 22, alpha = 0.05, type="pval", searchint = c(1.96,8)){
+get_thresh_analytic <- function(g, map, alpha = 0.05, type="pval", searchint = c(1.96,8)){
+  # calculate number of chromosomes
+  chr <- length(unique(example_map$chr))
+  # calculate distances between consecutive markers on same chromosome
+  delt.mat <- sapply(1:chr, function(x) diff(subset(example_map, chr == x)$cM))
+  # calculate marker density (average) and total length
+  delt <- mean(delt.mat)
+  L <- sum(delt.mat)
   # get test stat threshold
   Z <- uniroot(OU_approx, interval = searchint, beta = 0.01*g, Delta = delt, length = L, chr = chr, center = alpha, test = 'two-sided')$root
   # if type = 'pval', return pval instead
@@ -37,3 +41,4 @@ get_thresh_analytic <- function(g, delt, L, chr = 22, alpha = 0.05, type="pval",
     cat("Error: please specify type='stat' or 'pval'.")
   }
 }
+
