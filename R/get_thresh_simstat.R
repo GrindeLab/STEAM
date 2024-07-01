fac9c3dcff1d428c580ae16601d5124ca3a04ad9
sydneyohr <sydney.ohr@gmail.com>
2024-07-01 16:08:35 -0500

    added progress bar

--- R/get_thresh_simstat.R
+++ R/get_thresh_simstat.R
@@ -11,6 +11,7 @@
 #' @param nreps the number of repetitions for the simulation study; default is 10000.
 #' @param alpha the level for family-wise error rate control; default is 0.05.
 #' @param type the type of threshold that should be returned: \code{"stat"} for test statistic or \code{"pval"} for p-value; defaults to pval.
+#' @param method the method used to simulate test stat: \code{"cpp"} for cpp (using rcpp, 80 percent faster approximately) or \code{"r"} for r.
 #'
 #' @return A single number indicating the estimated significance threshold (either test statistic or p-value).
 #'
@@ -18,29 +19,49 @@
 #' get_thresh_simstat(g = 6, map = example_map, props = example_props, nreps = 10)
 #' get_thresh_simstat(g = 6, map = example_map, props = example_props, nreps = 10, type="stat")
 #'
-#' @importFrom stats quantile pnorm
+#' @importFrom stats quantile pnorm future
 #'
 #' @export
 
-get_thresh_simstat <- function(g, map, props, nreps=10000, alpha=0.05, type="pval"){
+get_thresh_simstat <- function(g, map, props, nreps=10000, alpha=0.05, type="pval", method = "cpp"){
   # get distances between adjacent markers
+  
+  
   dlt <- c(0,get_deltas(map)) # length m
-
+  
   # pre-calculate constants for test stat sim (a's and b's)
   ab <- get_ab(dlt,g)
-
+  
   # get average admixture proportions
   avg_props <- apply(props,2,mean,na.rm=T)
-
+  
   # calculate the matrix L
   L <- get_L(avg_props) # could condense with calculating avg
-
+  
+  handlers("txtprogressbar")
+  p <- progressr::progressor(steps = nreps)
+  
+  
   # simulate test stats nreps times
-  max_stats <- replicate(nreps, simstat_once(m = nrow(map), K = ncol(props), as = ab$a, bs = ab$b, L = L))
-
+  
+  
+  max_stats <- numeric(nreps)
+  for (i in 1:nreps) {
+    p()
+    if (method == "cpp") {
+      max_stats[i] <- simstatSingle(m = nrow(map), K = ncol(props), as = ab$a, bs = ab$b, L = L)
+    } else {
+      max_stats[i] <- simstat_once(m = nrow(map), K = ncol(props), as = ab$a, bs = ab$b, L = L)
+    }
+    
+  }
+  
+  message("loading...")
+  
+  
   # get upper alpha quantile
   zstar <- upper_alpha(max_stats, alpha)
-
+  
   # get 95% bootstrap CI for threshold (5k reps)
   if (requireNamespace("bootstrap", quietly = TRUE)) {
     z_ci <- quantile(bootstrap::bootstrap(max_stats, nboot = 5000, theta = upper_alpha, alpha)$thetastar, c(0.025,0.975))
@@ -48,7 +69,7 @@ get_thresh_simstat <- function(g, map, props, nreps=10000, alpha=0.05, type="pva
     z_ci <-c(NA,NA)
     cat('Install package \"bootstrap\" to get confidence interval. \n')
   }
-
+  
   # return threshold
   if(type == "stat"){
     thresh <- zstar
