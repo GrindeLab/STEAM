d2a2532e020b3c9507a551a4cafe0eadf09f1e87
sydneyohr <sydney.ohr@gmail.com>
2024-07-09 13:27:17 -0500

    Update get_thresh_simstat.R

--- R/get_thresh_simstat.R
+++ R/get_thresh_simstat.R
@@ -39,6 +39,7 @@ get_thresh_simstat <- function(g, map, props, nreps=10000, alpha=0.05, type="pva
   L <- get_L(avg_props) # could condense with calculating avg
   
   handlers(global = TRUE)
+  handlers("progress")
   p <- progressr::progressor(steps = nreps)
   
   
