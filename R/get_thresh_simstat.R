46ea0717a490ae256f487541783d8ac68f3abf6a
sydneyohr <sydney.ohr@gmail.com>
2024-07-10 11:19:45 -0500

    Update get_thresh_simstat.R
    testing progress bar

--- R/get_thresh_simstat.R
+++ R/get_thresh_simstat.R
@@ -38,24 +38,28 @@ get_thresh_simstat <- function(g, map, props, nreps=10000, alpha=0.05, type="pva
   # calculate the matrix L
   L <- get_L(avg_props) # could condense with calculating avg
   
-  handlers(global = TRUE)
   handlers("txtprogressbar")
-  p <- progressr::progressor(steps = nreps)
   
   
   # simulate test stats nreps times
 
-  
-  max_stats <- numeric(nreps)
-  for (i in 1:nreps) {
-    p()
-    if (method == "cpp") {
-      max_stats[i] <- simstatSingle(m = nrow(map), K = ncol(props), as = ab$a, bs = ab$b, L = L)
-    } else {
-      max_stats[i] <- simstat_once(m = nrow(map), K = ncol(props), as = ab$a, bs = ab$b, L = L)
+  with_progress({
+    
+    p <- progressr::progressor(steps = nreps)
+    
+    max_stats <- numeric(nreps)
+    for (i in 1:nreps) {
+      p()
+      if (method == "cpp") {
+        max_stats[i] <- simstatSingle(m = nrow(map), K = ncol(props), as = ab$a, bs = ab$b, L = L)
+      } else {
+        max_stats[i] <- simstat_once(m = nrow(map), K = ncol(props), as = ab$a, bs = ab$b, L = L)
+      }
+      
     }
     
-  }
+  })
+ 
   
   message("loading...")
   
