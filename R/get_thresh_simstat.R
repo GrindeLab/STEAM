70b8c24ed17d1162e3b834ff157bcd3932be8183
sydneyohr <sydney.ohr@gmail.com>
2024-07-10 15:23:57 -0500

    Update get_thresh_simstat.R

--- R/get_thresh_simstat.R
+++ R/get_thresh_simstat.R
@@ -56,7 +56,7 @@ get_thresh_simstat <- function(g, map, props, nreps=10000, alpha=0.05, type="pva
       } else {
         max_stats[i] <- simstat_once(m = nrow(map), K = ncol(props), as = ab$a, bs = ab$b, L = L)
       }
-      
+      flush.console()
     }
     
   })
