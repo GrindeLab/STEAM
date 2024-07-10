3461e547e8376eed8659c1882f7140504ce3ce81
sydneyohr <sydney.ohr@gmail.com>
2024-07-10 11:25:58 -0500

    Update get_thresh_simstat.R

--- R/get_thresh_simstat.R
+++ R/get_thresh_simstat.R
@@ -39,13 +39,12 @@ get_thresh_simstat <- function(g, map, props, nreps=10000, alpha=0.05, type="pva
   L <- get_L(avg_props) # could condense with calculating avg
   
   handlers("txtprogressbar")
-  
+  p <- progressr::progressor(steps = nreps)
   
   # simulate test stats nreps times
 
   with_progress({
     
-    p <- progressr::progressor(steps = nreps)
     
     max_stats <- numeric(nreps)
     for (i in 1:nreps) {
